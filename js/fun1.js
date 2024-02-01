aa06f8be0b4f649a02a46e75a6b8cf211826c133
Raushan <raushankumar23082004@gmail.com>
2024-02-01 16:20:16 +0530

    up

---
+++ js/fun1.js
@@ -0,0 +1,25 @@
+function poem()
+{
+    console.log("twinkle twinkle little star,how i wonder what do are:");
+}
+//poem();
+
+function roledice()
+{
+    console.log(Math.floor(Math.random()*6)+1);
+}
+// roledice();
+function printInfo(name,age)
+{
+console.log(`${name}'s age is ${age}:`)
+}
+// printInfo("raushan",78);
+// printInfo("raushan");
+function avg(a,b,c)
+{
+ return ((a+b+c)/3);
+}
+
+console.log(avg(3,3,3));
+
+// till 14 completed
