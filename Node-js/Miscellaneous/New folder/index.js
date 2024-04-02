e5e70280d11645921dbb9617253c222b2d5b1583
Raushan <raushankumar23082004@gmail.com>
2024-04-02 19:29:04 +0530

    delta

---
+++ Node-js/Miscellaneous/New folder/index.js
@@ -0,0 +1,18 @@
+const express=require("express");
+const app=express();
+const port=8085;
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+app.get("/register",(req,res)=>
+{
+    let {username,password}=req.query;
+    res.send(`getting response,welcome ${username}`);
+});
+app.post("/register",(req,res)=>
+{
+    let {username,password}=(req.body);
+    res.send(`psting response,welcome ${username} and password is:${password}`);
+});
+app.listen(port,()=>{
+    console.log(`Server is listening on port ${port}`);
+}); 
